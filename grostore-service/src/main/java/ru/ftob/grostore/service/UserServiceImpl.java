4f0ef5f2865f9e6f41e87ec3b58e005cec0ac06d
sergeymitrichev <sergeymitrichev@yandex.ru>
2018-07-11 18:46:07 +0300

    Add Ucoz Api module structure

---
+++ grostore-service/src/main/java/ru/ftob/grostore/service/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package ru.ftob.grostore.service;
+
+import ru.ftob.grostore.model.User;
+import ru.ftob.grostore.persistence.UserRepository;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+    private UserRepository repository;
+
+    @Override
+    public User get(int id) throws ClassNotFoundException {
+        return repository.get(id);
+    }
+
+    @Override
+    public User create(User entity) {
+        return repository.save(entity);
+    }
+
+    @Override
+    public void update(User entity) {
+        repository.save(entity);
+    }
+
+    @Override
+    public void delete(int id) throws ClassNotFoundException {
+        repository.delete(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return repository.getAll();
+    }
+}
