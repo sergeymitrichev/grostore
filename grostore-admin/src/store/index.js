7b1cddc753a3985c1c88841b2f5ab3f0803acb88
sergeymitrichev <sergeymitrichev@yandex.ru>
2018-07-20 17:56:05 +0300

    ADMIN init vuetify template

--- grostore-admin/src/store/index.js
+++ grostore-admin/src/store/index.js
@@ -1,5 +1,6 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
+import shop from "../api/shop";
 
 Vue.use(Vuex);
 
@@ -16,9 +17,16 @@ export default new Vuex.Store({
   },
 
   actions: {
-    fetchProducts () {
-      // make the call
-      // run serProducts mutation
+    fetchProducts ({commit}) {
+      return new Promise((resolve, reject) => {
+        // make the call
+        // run serProducts mutation
+        shop.getProducts(products => {
+          commit('setProducts', products)
+          resolve()
+        })
+      })
+
     }
   },
 
