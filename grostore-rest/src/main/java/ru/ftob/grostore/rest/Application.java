303623219fa9bd32e522db61e874cf99df1d950d
sergeymitrichev <sergeymitrichev@yandex.ru>
2018-07-27 12:29:25 +0300

    REST add file upload

--- grostore-rest/src/main/java/ru/ftob/grostore/rest/Application.java
+++ grostore-rest/src/main/java/ru/ftob/grostore/rest/Application.java
@@ -3,29 +3,21 @@ package ru.ftob.grostore.rest;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Arrays;
-
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
         };
     }
 }
--- grostore-rest/src/main/java/ru/ftob/grostore/rest/ProductController.java
+++ grostore-rest/src/main/java/ru/ftob/grostore/rest/ProductController.java
@@ -1,15 +1,47 @@
 package ru.ftob.grostore.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
 public class ProductController {
 
-    @RequestMapping("/import")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    private final StorageService storageService;
+
+    @Autowired
+    public ProductController(StorageService storageService) {
+        this.storageService = storageService;
     }
+
+    @PostMapping("/import")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
+
 
     // TODO upload files http://spring.io/guides/gs/uploading-files/
 }
