387d2015429e3550727f293b7803b126b2065678
sergeymitrichev <sergeymitrichev@yandex.ru>
2018-07-27 11:37:19 +0300

    ADMIN add layout structure

---
+++ grostore-admin/src/layouts/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app
+    id="inspire"
+    dark
+  >
+    <Navbar></Navbar>
+    <Content></Content>
+  </v-app>
+</template>
+
+<script>
+  import Navbar from "./Navbar";
+  import Content from "./Content";
+
+  export default {
+    name: "Layout",
+    components: {Content, Navbar}
+  }
+</script>
+
+<style scoped>
+
+</style>
