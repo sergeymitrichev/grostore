40ae6674ca5f5f7445f3cc365154a045211350e9
sergeymitrichev <sergeymitrichev@yandex.ru>
2018-07-27 11:37:19 +0300

    ADMIN add layout structure

---
+++ grostore-admin/src/components/products/ProductImportXls.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card flat>
+    <label>
+      Open file dialog
+      <input type="file" style="position: fixed; top: -100em">
+    </label>
+  </v-card>
+</template>
+
+<script>
+
+  export default {
+    name: "ProductImportXls"
+  }
+</script>
+
+<style scoped>
+label {
+  border: 1px solid color()
+}
+</style>
---
+++ grostore-admin/src/layouts/Content.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-content>
+    <v-container grid-list-md>
+      <v-layout row wrap>
+        <v-flex xs-12>
+          <router-view/>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+  export default {
+    name: "Content"
+  }
+</script>
+
+<style scoped>
+
+</style>
