e7124ef3221e2a20c8d2390087d2b2eda29aca5a
Sergey Mitrichev <sergeymitrichev@yandex.ru>
2018-10-02 00:18:48 +0300

    Add product service import

--- grostore-scheduler/src/main/java/ru/ftob/grostore/scheduler/task/ScheduledTask.java
+++ grostore-scheduler/src/main/java/ru/ftob/grostore/scheduler/task/ScheduledTask.java
@@ -1,13 +1,24 @@
 package ru.ftob.grostore.scheduler.task;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.ftob.grostore.model.product.Product;
 import ru.ftob.grostore.scheduler.MetroCatalogParseResult;
+import ru.ftob.grostore.service.product.ProductService;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
+@Component
 public class ScheduledTask<V> implements RunnableScheduledFuture<V> {
+
+    @Autowired
+    private ProductService productService;
+
     @Override
     public boolean isPeriodic() {
         return false;
@@ -32,7 +43,8 @@ public class ScheduledTask<V> implements RunnableScheduledFuture<V> {
         try {
             MetroCatalogParseResult result = mapper.readValue(new URL(url), MetroCatalogParseResult.class);
             System.out.println("Parsed products");
-            result.getItems().forEach(System.out::println);
+            List<Product> products = result.getItems().stream().map(i -> mapper.convertValue(i, Product.class)).collect(Collectors.toList());
+            productService.updateAll(products);
 
         } catch (IOException e) {
             e.printStackTrace();
