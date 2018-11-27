fddac973c6507645bab1d34e1017207365623f52
sergeymitrichev <sergeymitrichev@yandex.ru>
2018-11-27 19:18:59 +0300

    Rest controller refactoring

--- grostore-rest/src/main/java/ru/ftob/grostore/rest/controller/AbstractRestController.java
+++ grostore-rest/src/main/java/ru/ftob/grostore/rest/controller/AbstractRestController.java
@@ -1,12 +1,62 @@
 package ru.ftob.grostore.rest.controller;
 
-import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.ftob.grostore.rest.util.ModelMapperUtils;
+import ru.ftob.grostore.service.BaseService;
 
-public class AbstractRestController {
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
 
-    private final ModelMapper mapper = new ModelMapper();
+public class AbstractRestController<T, ID, G> {
 
-    public ModelMapper getMapper() {
-        return mapper;
+    private Class<G> guiClass;
+
+    private Class<T> dbClass;
+
+    private BaseService<T, ID> service;
+
+    public AbstractRestController() {
+        this.dbClass = (Class<T>) ((ParameterizedType) getClass()
+                .getGenericSuperclass()).getActualTypeArguments()[0];
+        this.guiClass = (Class<G>) ((ParameterizedType) getClass()
+                .getGenericSuperclass()).getActualTypeArguments()[2];
+    }
+
+    public void setService(BaseService<T, ID> service) {
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> getAll(Pageable pageable) {
+        return ResponseEntity.ok(ModelMapperUtils.mapPage(service.getAll(pageable), guiClass, pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> get(@PathVariable ID id) {
+        return ResponseEntity.ok(ModelMapperUtils.map(service.get(id), guiClass));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody G guiEntity) {
+        T dbEntity = ModelMapperUtils.map(guiEntity, dbClass);
+        return ResponseEntity.ok(ModelMapperUtils.map(service.create(dbEntity), guiClass));
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<?> update(
+            @PathVariable ID id,
+            @RequestBody G guiEntity
+    ) {
+        T dbEntity = service.update(ModelMapperUtils.map(guiEntity, dbClass));
+        return ResponseEntity.ok(ModelMapperUtils.map(dbEntity, guiClass));
+    }
+
+    @DeleteMapping("/")
+    public ResponseEntity<?> deleteAll(@RequestBody List<G> guiList) {
+        List<T> dbList = ModelMapperUtils.mapAll(guiList, dbClass);
+        service.deleteAll(dbList);
+        return ResponseEntity.ok().build();
     }
 }
