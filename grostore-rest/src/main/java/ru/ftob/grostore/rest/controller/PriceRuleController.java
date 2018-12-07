285885567641cf0bca8fbbbfc921b55bee65c69e
sergeymitrichev <sergeymitrichev@yandex.ru>
2018-12-07 12:39:44 +0300

    Add endpoints for prices

--- grostore-rest/src/main/java/ru/ftob/grostore/rest/controller/PriceRuleController.java
+++ grostore-rest/src/main/java/ru/ftob/grostore/rest/controller/PriceRuleController.java
@@ -1,19 +1,33 @@
 package ru.ftob.grostore.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.ftob.grostore.model.product.PriceRule;
 import ru.ftob.grostore.rest.webmodel.GuiPriceRule;
+import ru.ftob.grostore.service.price.PriceRuleServiceImpl;
+import ru.ftob.grostore.service.price.PriceService;
 
 @RestController
 @RequestMapping("/price-rules")
 public class PriceRuleController extends AbstractRestController<PriceRule, Integer, GuiPriceRule> {
-    private PriceRuleService service;
+
+    private PriceRuleServiceImpl service;
+
+    private PriceService priceService;
 
     @Autowired
-    public StockController(PriceRuleService service) {
+    public PriceRuleController(PriceRuleServiceImpl service, PriceService priceService) {
+        this.priceService = priceService;
         this.service = service;
         setService(service);
+    }
+
+    @PostMapping("/calculate")
+    public ResponseEntity<?> calculate() {
+        priceService.generateOutFromAllProducts();
+        return ResponseEntity.ok().build();
     }
 }
--- grostore-rest/src/main/java/ru/ftob/grostore/rest/webmodel/GuiPriceRule.java
+++ grostore-rest/src/main/java/ru/ftob/grostore/rest/webmodel/GuiPriceRule.java
@@ -1,4 +1,26 @@
 package ru.ftob.grostore.rest.webmodel;
 
+import ru.ftob.grostore.model.product.PriceType;
+
+import java.util.Set;
+
 public class GuiPriceRule {
+    private Set<GuiPriceRuleRow> rows;
+    private Set<PriceType> types;
+
+    public Set<GuiPriceRuleRow> getRows() {
+        return rows;
+    }
+
+    public void setRows(Set<GuiPriceRuleRow> rows) {
+        this.rows = rows;
+    }
+
+    public Set<PriceType> getTypes() {
+        return types;
+    }
+
+    public void setTypes(Set<PriceType> types) {
+        this.types = types;
+    }
 }
