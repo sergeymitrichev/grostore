9f61156f64b84a0255e1ac5f7cad1ad7eb520f74
sergeymitrichev <sergeymitrichev@yandex.ru>
2018-12-18 17:40:50 +0300

    Add dynamic nested routes for categories

---
+++ grostore-frontend/pages/shop/_.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout>
+    {{ categories[0].title }}
+  </v-layout>
+</template>
+
+<script>
+export default {
+  layout: 'catalog',
+  data() {
+    return {
+      categories: [
+        {
+          id: 1,
+          title: 'Овощи, грибы, фрукты, зелень',
+          to: '/url1/1'
+        }
+      ]
+    }
+  }
+}
+</script>
