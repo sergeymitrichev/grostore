dda8b34450184337f99f022cb7ca7d5cff0c4d15
sergeymitrichev <sergeymitrichev@yandex.ru>
2019-01-14 17:50:34 +0300

    Change admin module api url params

--- grostore-admin/config/index.js
+++ grostore-admin/config/index.js
@@ -11,15 +11,15 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/': {
-        target: 'http://localhost:6100',
+      '/api': {
+        target: 'http://195.161.41.140:49341',
         changeOrigin: true
       }
     },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 6200, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
--- grostore-admin/src/services/product.service.js
+++ grostore-admin/src/services/product.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = '/';
+const BASE_URL = '/api/';
 
 export default class ProductService {
 
